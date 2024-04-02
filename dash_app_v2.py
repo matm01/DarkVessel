14dbefd73edd5df1cdf15f2dbc75f30079771c15
Jordan Vinck <jovinck+git@proton.me>
2024-04-02 20:49:22 +0200

    Add vessel details or prediction details and picture on data click in a side panel (right)

--- dash_app_v2.py
+++ dash_app_v2.py
@@ -18,7 +18,7 @@ unique_dates = df_timestamp['DATE'].unique()
 list_of_unique_dates = unique_dates.tolist()
 
 # Results from the predictions
-results_file = 'results/sample_results.csv'
+results_file = 'results/results_all.csv'
 df_results = pd.read_csv(results_file)
 
 # Map
@@ -73,11 +73,39 @@ interactive_map = dbc.Row([
         dcc.Graph(
             id='base-map',
             config={'displayModeBar': False},
-            # clickData=None
+            clickData=None
         )
     ])
 ])
 
+ship_report = dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(
+                id='click-output-data',
+                columns=[
+                    {"name": "Attribute", "id": "Attribute"}, 
+                    {"name": "Value", "id": "Value"}
+                ],
+                style_cell_conditional=[
+                    {'if': {'column_id': 'Attribute'}, 'width': '60px'},
+                    {'if': {'column_id': 'Value'}, 'width': '240px'}
+                ],
+                style_cell={
+                    'textAlign': 'left', 
+                    'minWidth': '60px',
+                    'maxWidth': '240px',
+                    'whiteSpace': 'normal'
+                },
+                style_data=dict(height='20px'),
+                style_table={'overflowX': 'auto'},
+            ),
+            html.Img(
+                id='image-placeholder', 
+                alt='Click on data point to display image',
+                style={'width': '290px', 'height': '300px'}
+            ),
+        ], style={'top': '5rem'})
+])
 
 
 #==============================================================================
@@ -104,14 +132,9 @@ app.layout = dbc.Container([
                 interactive_map,
                 ],width=8
             ),
-            # dbc.Col(
-            #     dbc.Row([
-            #         dbc.Col(dbc.Button('Previous', id='prev-btn', n_clicks=0), width=1),
-            #         dbc.Col(dbc.Button('Next', id='next-btn', n_clicks=0), width=1),
-            #         dbc.Col(html.Div(id='frame-date'), width=1),
-            #     ]), width=2
-
-            # ),
+            dbc.Col([
+                ship_report,
+            ], width=2, style={"background-color": "#f8f9fa", "top": "2rem", "padding": "2rem 0rem"}),
         ]),
     ], fluid=True
 )
@@ -136,21 +159,21 @@ def update_end_date_options(start_date):
 # Filter results based on date range
 @app.callback(
     Output('data-table', 'data'),
+    # Output('frame-date', 'children')],
     Input('filter-button', 'n_clicks'),
     State('start-date', 'value'),
     State('end-date', 'value')
 )
 def update_data_table(n_clicks, start_date, end_date):
     if start_date and end_date:
-        mask_dates = (df_results['DATE'] >= start_date) & (df_results['DATE'] <= end_date)
+        mask_dates = (df_results['date'] >= start_date) & (df_results['date'] <= end_date)
         filtered_df = df_results[mask_dates]
-        filtered_df = filtered_df.sort_values('DATE')
+        filtered_df = filtered_df.sort_values('date')
         data = filtered_df.to_dict('records')
-        # print(data)
-        return data
+        return data  #, start_date
     else:
         data = df_results.to_dict('records')
-        return None
+        return None  #, start_date
 
 #==============================================================================
 
@@ -166,7 +189,6 @@ def update_data_table(n_clicks, start_date, end_date):
 def update_map_date(prev_clicks, next_clicks, start_date, end_date, frame_date):
     mask_dates = (unique_dates >= start_date) & (unique_dates <= end_date)
     filtered_dates = unique_dates[mask_dates].tolist()
-    
     ctx = callback_context
     if not ctx.triggered or not filtered_dates:
         return filtered_dates[0] if filtered_dates else None
@@ -179,8 +201,8 @@ def update_map_date(prev_clicks, next_clicks, start_date, end_date, frame_date):
             new_index = max(0, current_index - 1)
         else:
             new_index = current_index
-        print(filtered_dates[new_index])
-        print(filtered_dates)
+        # print(filtered_dates[new_index])
+        # print(filtered_dates)
         return filtered_dates[new_index]
 
 #==============================================================================
@@ -193,7 +215,7 @@ def update_map_date(prev_clicks, next_clicks, start_date, end_date, frame_date):
 )
 def update_map(frame_date, data):
     df_results = pd.DataFrame(data)
-    mask_date = df_results['DATE'] == frame_date
+    mask_date = df_results['date'] == frame_date
     df_results = df_results[mask_date]
     if data:
         # print(f'Updating map on clicks with {data}')
@@ -206,21 +228,21 @@ def update_map(frame_date, data):
             zoom=9,
             height=700,
             # mapbox_style="carto-positron",
-            hover_data=['name', 'latitude', 'longitude', 'prediction', 'image']
+            hover_data=['latitude', 'longitude', 'mmsi', 'name', 'country', 'timestamp', 'timedelta', 'prediction', 'image', 'date']
         )
         fig.update_layout(
             # mapbox_bounds={"west": 22.35, "east": 23.12, "south": 36.35, "north": 36.85},
             margin={"r": 5, "t": 5, "l": 5, "b": 5},
             # mapbox_style="streets",
-            mapbox_style="satellite-streets",
-            # mapbox_style="mapbox://styles/mapbox/navigation-guidance-night-v2",
+            # mapbox_style="satellite-streets",
+            mapbox_style="mapbox://styles/mapbox/navigation-guidance-night-v2",
             mapbox_accesstoken=token
         )
         fig.update(layout_coloraxis_showscale=False)
         fig.update_mapboxes(center=dict(lat=latitude, lon=longitude))
         return fig
     # else:
-    print(f'Update map no clicks: {data}')
+    # print(f'Update map no clicks: {data}')
     fig = px.scatter_mapbox(
         lat=[latitude],
         lon=[longitude],
@@ -247,6 +269,73 @@ def update_map(frame_date, data):
 
 #==============================================================================
 
+def decimal_to_dms_latitude(decimal_lat):
+    is_negative = decimal_lat < 0
+    decimal_lat = abs(decimal_lat)
+    degrees = int(decimal_lat)
+    minutes = int((decimal_lat - degrees) * 60)
+    seconds = (decimal_lat - degrees - minutes/60) * 3600
+    direction = "S" if is_negative else "N"
+    return f"{degrees}°{minutes}'{seconds:.1f}\"{direction}"
+
+def decimal_to_dms_longitude(decimal_lon):
+    is_negative = decimal_lon < 0
+    decimal_lon = abs(decimal_lon)
+    degrees = int(decimal_lon)
+    minutes = int((decimal_lon - degrees) * 60)
+    seconds = (decimal_lon - degrees - minutes/60) * 3600
+    direction = "W" if is_negative else "E"
+    return f"{degrees}°{minutes}'{seconds:.1f}\"{direction}"
+
+
+@app.callback(
+    Output('click-output-data', 'data'),
+    Input('base-map', 'clickData'))
+def display_click_data_table(clickData):
+    if clickData is None:
+        return [{}]
+    else:
+        point_data = clickData['points'][0]
+        lat = point_data.get('customdata', [None])[0]
+        lat_formatted = decimal_to_dms_latitude(lat)
+        lon = point_data.get('customdata', [None])[1]
+        lon_formatted = decimal_to_dms_longitude(lon)
+        mmsi = point_data.get('customdata', [None])[2]
+        name = point_data.get('customdata', [None])[3]
+        country = point_data.get('customdata', [None])[4]
+        timestamp = point_data.get('customdata', [None])[5]
+        timedelta = point_data.get('customdata', [None])[6]
+        prediction = point_data.get('customdata', [None])[7]
+        image = point_data.get('customdata', [None])[8]
+        data = [
+            {"Attribute": "Date", "Value": timestamp},
+            {"Attribute": "Prediction", "Value": prediction},
+            {"Attribute": "Latitude", "Value": lat_formatted},
+            {"Attribute": "Longitude", "Value": lon_formatted},
+            {"Attribute": "MMSI", "Value": mmsi},
+            {"Attribute": "Ship Name", "Value": name},
+            {"Attribute": "Country", "Value": country},
+            {"Attribute": "Time Delta", "Value": timedelta},
+            # {"Attribute": "Image", "Value": image}
+        ]
+    return data
+
+
+@app.callback(
+    Output('image-placeholder', 'src'),
+    Input('base-map', 'clickData'))
+def display_click_data_image(clickData):
+    if clickData is None:
+        return ''
+    else:
+        try:
+            point_data = clickData['points'][0]
+            image = point_data.get('customdata', [None])[8]
+            return image
+        except IndexError:
+            return ''
+
+#==============================================================================
 
 
 
