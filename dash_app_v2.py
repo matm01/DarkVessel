12f1ef3892458d8eb6877639cb4d624ad867e265
Jordan Vinck <jovinck+git@proton.me>
2024-04-02 23:49:31 +0200

    Add summary statistics for the selected dates

--- dash_app_v2.py
+++ dash_app_v2.py
@@ -1,13 +1,14 @@
 from dash import Dash, html, dcc, callback, Output, Input, State, dash_table, callback_context
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
+import numpy as np
 import os
 import pandas as pd
 import plotly.express as px
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
-load_figure_template('SPACELAB')
+app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
+load_figure_template('SOLAR')
 
 
 # Timestamps for existing SAR images
@@ -26,45 +27,68 @@ token = os.environ.get('MAPBOX_TOKEN')
 latitude = 36.53353  # 36.34289 
 longitude = 22.721728  # 22.43289
 
-
 #==============================================================================
 # Define the content of the app
 #==============================================================================
 
 header = dbc.Row([
     dbc.Col([
-        html.H1("Ship-to-Ship (STS) Transfer Detection in SAR images", id='header')
+        html.H1("Ship-to-Ship (STS) Transfer Detection in SAR images", id='header', className="mb-4")
     ])
 ])
 
 sidebar = dbc.Row([
     dbc.Col([
-        html.H2("Sidebar", className="display-4"),
+        # html.H2("Sidebar", className="display-4"),
         html.Hr(),
-        html.P(
-            "A simple sidebar layout with navigation links", className="lead"
-        ),
+        html.P("Select start date", className="lead"),
         dcc.Dropdown(
             id='start-date',
             options=[{'label': date, 'value': date} for date in list_of_unique_dates],
             value=list_of_unique_dates[0] if list_of_unique_dates else None, # Sets the default value to the first date
         ),
+        html.Br(),
+        html.P("Select end date", className="lead"),
         dcc.Dropdown(
             id='end-date',
             options=[{'label': date, 'value': date} for date in list_of_unique_dates],
             value=list_of_unique_dates[1] if list_of_unique_dates else None, # Sets the default value to the first date
         ),
+        html.Br(),
         dbc.Button("Filter", id='filter-button', n_clicks=0),
-        dcc.Store(id='data-table')
+        html.Br(),
+        html.Hr(),
+        html.P("Summary Statistics", className="lead"),
+        dash_table.DataTable(
+            id='summary-table',
+            columns=[
+                {"name": "Description", "id": "description"}, 
+                {"name": "", "id": "statistics"}
+            ],
+            style_cell_conditional=[
+                {'if': {'column_id': 'Description'}, 'width': '250px'},
+                {'if': {'column_id': 'Statistics'}, 'width': '50px'}
+            ],
+            style_cell={
+                'textAlign': 'left', 
+                'minWidth': '50px',
+                'maxWidth': '250px',
+                'whiteSpace': 'normal'
+            },
+            style_data=dict(height='20px'),
+            style_table={'overflowX': 'auto'},
+            # style_header={'display': 'none'}
+        ),        
+        dcc.Store(id='data-table'),
     ])
 ])
 
 control = dbc.Row([
+    dbc.Col(html.Div(id='frame-date'), width=2),
     dbc.Col(dbc.ButtonGroup([
         dbc.Button('Previous', id='prev-btn', n_clicks=0),
         dbc.Button('Next', id='next-btn', n_clicks=0),
-    ]), width=2),
-    dbc.Col(html.Div(id='frame-date'), width=2),
+    ]), style={'width': 5, 'align':'middle'}),
 ])
 
 
@@ -80,14 +104,15 @@ interactive_map = dbc.Row([
 
 ship_report = dbc.Row([
         dbc.Col([
+            html.P("Vessel details", className="lead"),
             dash_table.DataTable(
                 id='click-output-data',
                 columns=[
-                    {"name": "Attribute", "id": "Attribute"}, 
+                    {"name": "Feature", "id": "Feature"}, 
                     {"name": "Value", "id": "Value"}
                 ],
                 style_cell_conditional=[
-                    {'if': {'column_id': 'Attribute'}, 'width': '60px'},
+                    {'if': {'column_id': 'Feature'}, 'width': '60px'},
                     {'if': {'column_id': 'Value'}, 'width': '240px'}
                 ],
                 style_cell={
@@ -98,6 +123,7 @@ ship_report = dbc.Row([
                 },
                 style_data=dict(height='20px'),
                 style_table={'overflowX': 'auto'},
+                # style_header={'display': 'none'}
             ),
             html.Img(
                 id='image-placeholder', 
@@ -107,38 +133,38 @@ ship_report = dbc.Row([
         ], style={'top': '5rem'})
 ])
 
-
 #==============================================================================
 # Define the layout of the app
 #==============================================================================
-
 
 app.layout = dbc.Container([
         dbc.Row([
             dbc.Col(
                 header,
                 width=12,
-                style={'textAlign': 'left', "background-color": "#f8f9fa", "padding": "1rem 1rem"}
+                style={'textAlign': 'left', "background-color": "#f8f9fa", "padding": "1rem 2rem"}
             ),
         ]),
         # Sidebar
         dbc.Row([
             dbc.Col(
                 sidebar,
-                width=2
+                width=2, style={'textAlign': 'left', "background-color": "#f8f9fa", "padding": "3rem 1rem"},
             ),
             dbc.Col([
                 control,
                 interactive_map,
-                ],width=8
+                ],width=8, style={"background-color": "#f8f9fa", "padding": "1rem 0rem"}
             ),
             dbc.Col([
                 ship_report,
-            ], width=2, style={"background-color": "#f8f9fa", "top": "2rem", "padding": "2rem 0rem"}),
+                ], width=2, style={"background-color": "#f8f9fa", "padding": "2rem 0rem"}),
         ]),
     ], fluid=True
 )
 
+#==============================================================================
+# Define the app callbacks
 #==============================================================================
 
 # Update end date options based on start date
@@ -163,7 +189,6 @@ def update_end_date_options(start_date):
 # Filter results based on date range
 @app.callback(
     Output('data-table', 'data'),
-    # Output('frame-date', 'children')],
     Input('filter-button', 'n_clicks'),
     Input('start-date', 'value'),
     State('end-date', 'value')
@@ -174,11 +199,43 @@ def update_data_table(n_clicks, start_date, end_date):
         filtered_df = df_results[mask_dates]
         filtered_df = filtered_df.sort_values('date')
         data = filtered_df.to_dict('records')
-        return data  #, start_date
+        return data
     else:
         data = df_results.to_dict('records')
-        return None  #, start_date
+        return None
 
+#==============================================================================
+
+# Filter results based on date range
+@app.callback(
+    Output('summary-table', 'data'),
+    Input('filter-button', 'n_clicks'),
+    State('data-table', 'data'),
+    State('start-date', 'value'),
+    State('end-date', 'value')
+)
+def update_data_table(n_clicks, data, start_date, end_date):
+    start = pd.to_datetime(start_date)
+    end = pd.to_datetime(end_date)
+    if n_clicks and data:
+        df_filtered = pd.DataFrame(data)
+        sts_detected =  df_filtered['prediction'].value_counts().loc['sts']
+        ship_detected =  df_filtered['prediction'].value_counts().loc['ship']
+        number_of_days = (end - start)/ np.timedelta64(1, 'D')
+        available_images = df_filtered['date'].nunique()
+        revisit_frequency = 1 / (available_images / number_of_days)
+        stats = [
+            {"description": "STS detected", "statistics": sts_detected},
+            {"description": "Ship detected", "statistics": ship_detected},
+            {"description": "Period (days)", "statistics": number_of_days},
+            {"description": "SAR images", "statistics": available_images},
+            {"description": "Revisit frequency", "statistics": round(revisit_frequency, 2)},
+            
+        ]
+        return stats  #, start_date
+    else:
+        data = [{}]
+        return data  #, start_date
 #==============================================================================
 
 # Update date for map based on frame selection using previous and next buttons
@@ -205,8 +262,6 @@ def update_map_date(prev_clicks, next_clicks, start_date, end_date, frame_date):
             new_index = max(0, current_index - 1)
         else:
             new_index = current_index
-        # print(filtered_dates[new_index])
-        # print(filtered_dates)
         return filtered_dates[new_index]
 
 #==============================================================================
@@ -229,14 +284,17 @@ def update_map(frame_date, data):
             lat="latitude",
             lon="longitude",
             color="prediction",
-            zoom=9,
+            zoom=10,
             height=700,
-            # mapbox_style="carto-positron",
             hover_data=['latitude', 'longitude', 'mmsi', 'name', 'country', 'timestamp', 'timedelta', 'prediction', 'image', 'date']
         )
+        fig.update_traces(
+            marker=dict(size=8, symbol="circle"),
+            selector=dict(mode="markers"),
+        )
         fig.update_layout(
-            # mapbox_bounds={"west": 22.35, "east": 23.12, "south": 36.35, "north": 36.85},
             margin={"r": 5, "t": 5, "l": 5, "b": 5},
+            # mapbox_style="carto-positron",
             # mapbox_style="streets",
             # mapbox_style="satellite-streets",
             mapbox_style="mapbox://styles/mapbox/navigation-guidance-night-v2",
@@ -245,24 +303,18 @@ def update_map(frame_date, data):
         fig.update(layout_coloraxis_showscale=False)
         fig.update_mapboxes(center=dict(lat=latitude, lon=longitude))
         return fig
-    # else:
-    # print(f'Update map no clicks: {data}')
+    
     fig = px.scatter_mapbox(
         lat=[latitude],
         lon=[longitude],
-        zoom=9,
+        zoom=8,
         height=700,
-        mapbox_style='carto-positron',  # open-street-map
     )
     fig.update_traces(
-        marker=dict(
-            size=0, # Adjust the size as needed
-            symbol="circle", # Set symbol to 'circle'
-        ),
+        marker=dict(size=0, symbol="circle"),
         selector=dict(mode="markers"),
     )
     fig.update_layout(
-        # mapbox_bounds={"west": 22.35, "east": 23.12, "south": 36.35, "north": 36.85},
         margin={"r": 5, "t": 5, "l": 5, "b": 5},
         mapbox_style="mapbox://styles/mapbox/navigation-guidance-night-v2",
         mapbox_accesstoken=token
@@ -281,6 +333,7 @@ def decimal_to_dms_latitude(decimal_lat):
     seconds = (decimal_lat - degrees - minutes/60) * 3600
     direction = "S" if is_negative else "N"
     return f"{degrees}°{minutes}'{seconds:.1f}\"{direction}"
+
 
 def decimal_to_dms_longitude(decimal_lon):
     is_negative = decimal_lon < 0
@@ -310,17 +363,17 @@ def display_click_data_table(clickData):
         timestamp = point_data.get('customdata', [None])[5]
         timedelta = point_data.get('customdata', [None])[6]
         prediction = point_data.get('customdata', [None])[7]
-        image = point_data.get('customdata', [None])[8]
+        # image = point_data.get('customdata', [None])[8]
         data = [
-            {"Attribute": "Date", "Value": timestamp},
-            {"Attribute": "Prediction", "Value": prediction},
-            {"Attribute": "Latitude", "Value": lat_formatted},
-            {"Attribute": "Longitude", "Value": lon_formatted},
-            {"Attribute": "MMSI", "Value": mmsi},
-            {"Attribute": "Ship Name", "Value": name},
-            {"Attribute": "Country", "Value": country},
-            {"Attribute": "Time Delta", "Value": timedelta},
-            # {"Attribute": "Image", "Value": image}
+            {"Feature": "Date", "Value": timestamp},
+            {"Feature": "Prediction", "Value": prediction},
+            {"Feature": "Latitude", "Value": lat_formatted},
+            {"Feature": "Longitude", "Value": lon_formatted},
+            {"Feature": "MMSI", "Value": mmsi},
+            {"Feature": "Ship Name", "Value": name},
+            {"Feature": "Country", "Value": country},
+            {"Feature": "Time Delta", "Value": timedelta},
+            # {"Feature": "Image", "Value": image}
         ]
     return data
 
